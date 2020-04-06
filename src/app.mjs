31245d9cdabfd6e4d296ea13fe11063dfac1e2e9
jascha <jascha@jaeh.at>
2020-04-06 11:28:42 +0200

    app: add seo

--- src/app.mjs
+++ src/app.mjs
@@ -1,4 +1,11 @@
 export const state = {
-  title: '8aces',
-  description: 'ethical incentive generates healthy traders - auto correcting economical system',
+  title: 'eight aces',
+  description: 'ethical incentive generates healthy traders - autonomously correcting economical system',
+
+  seo: {
+    name: 'eight aces',
+    url: `https://8aces.org/`,
+    about: 'ethical incentive generates healthy traders - autonomously correcting economical system',
+    image: `https://8aces.org/8aces-preview.jpg`,
+  },
 }
